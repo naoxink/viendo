961e92f831f10053bd24adf2fc1fa7f00bae1526
J. Gabriel Corpas <naoxink@icloud.com>
2026-07-10 06:23:39 +0000

    Fix api update

--- api/update.js
+++ api/update.js
@@ -84,7 +84,7 @@ export default async function handler(req, res) {
     // 3. Guardar
     await octokit.repos.createOrUpdateFileContents({
       owner: 'naoxink', repo: 'viendo', path,
-      message: `Update: ${serieNombre} T${temporada} E${capitulo}`,
+      message: `Update: ${serie.titulo} T${serie.temporada} E${serie.capitulo}`,
       content: Buffer.from(JSON.stringify(content, null, 2)).toString('base64'),
       sha: fileData.sha
     });
