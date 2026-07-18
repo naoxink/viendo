81cfde7498c70bfe518abdb6a48e6d2b4e693efe
J. Gabriel Corpas <naoxink@icloud.com>
2026-07-18 10:35:19 +0200

    Attribution thetvdb e ID de Ink Master

--- src/components/ViendoList.js
+++ src/components/ViendoList.js
@@ -16,6 +16,10 @@ export default {
         <div id="series-list" class="grid">
             <div class="no-content" v-if="!ordenada.length">No estás viendo nada actualmente :(</div>
             <ViendoCard v-for="serie in ordenada" :key="serie.titulo + serie.año" :serie="serie" />
+            <a class="thetvdbattribution" style="" href="https://thetvdb.com/subscribe">
+                    <img src="https://www.thetvdb.com/images/attribution/logo1.png" height="45">
+                    Metadata provided by TheTVDB. Please consider adding missing information or subscribing.
+            </a>
         </div>
     `
 }
