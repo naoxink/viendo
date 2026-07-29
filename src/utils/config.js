71d679fbc7a6e546ff467e5ec94c57d0e801ea1f
J. Gabriel Corpas <naoxink@icloud.com>
2026-07-29 07:53:40 +0000

    Añadida URL API https

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
   // Cambia esto por la URL real de tu backend en Vercel
-  API_BASE_URL: 'http://naoxink-viendo.vercel.app' 
+  API_BASE_URL: 'https://naoxink-viendo.vercel.app/' 
 };
